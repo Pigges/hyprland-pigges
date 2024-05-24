1f7ea77dbe1a0169706968a7d8c8a61e7942a5e2
Pigges <contact@pigg.es>
2024-05-24 23:39:44 +0200

    move to hypr directory

---
+++ autostart.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+source ~/.config/hypr/lib.sh
+
+
+# Configuration stuff
+wl-clipboard-history -t &
+~/.config/hypr/xdg-portal-hyprland &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+wlsunset -S 9:00 -s 19:30 &
+wl-paste -p --watch wl-copy -pc &
+
+# Sort of fix wayland screensharing
+#xwaylandvideobridge &
+
+# Wallpaper
+swww-daemon &
+#swaybg -m fill -i ~/Pictures/Wallpapers/hyprfog-2.png &
+set_wallpaper $(find ~/Pictures/Wallpapers/ -type f | shuf -n 1)
+
+# Reload Hyprland plugins
+hyprpm reload &
+
+# Applications
+# waybar &
+~/.config/hypr/waybar/start &
+nm-applet &
+blueman-applet &
+
+brightnessctl --restore &
+
+~/.config/hypr/scripts/configure-split-monitor-workspaces
